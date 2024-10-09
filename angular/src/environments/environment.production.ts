b5337103b02edf9b5b97f857fcce26fad1b2dc5c
felipe <philip@worldready.io>
2024-10-09 03:39:55 +0000

    final fix for vercel

--- angular/src/environments/environment.production.ts
+++ angular/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vercel.live/link/limpinplus.vercel.app/api/v1'  // Replace with your actual production API URL
+  apiUrl: 'https://limpinplus.vercel.app/api/v1'  // Replace with your actual production API URL
 };
